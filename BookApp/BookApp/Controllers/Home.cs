ae4c9b7394fffae5a62d7d3dd66742efab6f419a
Peter Larsen <pla@knowledgecube.net>
2021-05-12 21:39:45 +0200

    Adding routing default page

---
+++ BookApp/BookApp/Controllers/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class Home : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            // Dont think this is th right way to go, but routing needs default index from homecontroller. Trying to remove error.
+            // This is pure backend-api, and we dont need a frontend webpage
+            return View();
+        }
+    }
+}
